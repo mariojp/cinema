230f27180961fb822b0eeb60ee2ec063c0c0674e
Mario Pereira <554178+mariojp@users.noreply.github.com>
2022-05-05 11:21:07 -0300

    parte 1

--- src/main/java/br/com/mariojp/cinema/Principal.java
+++ src/main/java/br/com/mariojp/cinema/Principal.java
@@ -3,7 +3,24 @@ package br.com.mariojp.cinema;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[][] cinema = new String[7][8];
+
+		cinema[0][0] = " ";
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 8; j++) {
+				cinema[i][j] = "S";
+			}
+		}
+
+		System.out.println("Cinema:");
+		System.out.println("  1 2 3 4 5 6 7 8");
+		for (int i = 0; i < 7; i++) {
+			System.out.print((i + 1) + " ");
+			for (int j = 0; j < 8; j++) {
+				System.out.print(cinema[i][j] + " ");
+			}
+			System.out.println();
+		}
 
 	}
 
