8f53563fc6b690165465dd79970276dcc44ed3bd
Mario Pereira <554178+mariojp@users.noreply.github.com>
2022-05-05 14:37:16 -0300

    parte 2

--- src/main/java/br/com/mariojp/cinema/Principal.java
+++ src/main/java/br/com/mariojp/cinema/Principal.java
@@ -1,26 +1,54 @@
 package br.com.mariojp.cinema;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		String[][] cinema = new String[7][8];
+
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Numero de Filas: ");
+		int fileiras = Integer.parseInt(teclado.nextLine());
+
+		System.out.println("Assentos por Filas:");
+		int assentosPorFileira = Integer.parseInt(teclado.nextLine());
+
+		String[][] cinema = new String[fileiras][assentosPorFileira];
 
 		cinema[0][0] = " ";
-		for (int i = 0; i < 7; i++) {
-			for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < fileiras; i++) {
+			for (int j = 0; j < assentosPorFileira; j++) {
 				cinema[i][j] = "S";
 			}
 		}
 
 		System.out.println("Cinema:");
-		System.out.println("  1 2 3 4 5 6 7 8");
-		for (int i = 0; i < 7; i++) {
+		String fila = " ";
+		for( int i = 1; i <= assentosPorFileira; i++ )  fila = fila +" "+i; 
+		System.out.println(fila);
+		for (int i = 0; i < fileiras; i++) {
 			System.out.print((i + 1) + " ");
-			for (int j = 0; j < 8; j++) {
+			for (int j = 0; j < assentosPorFileira; j++) {
 				System.out.print(cinema[i][j] + " ");
 			}
 			System.out.println();
 		}
+
+		int assentos = fileiras * assentosPorFileira;
+		int total;
+
+		if (assentos <= 60) {
+			total = assentos * 10;
+		} else {
+			if (fileiras % 2 == 0) {
+				total = (assentos / 2) * 10 + (assentos / 2) * 8;
+			} else {
+				total = (fileiras / 2 * assentosPorFileira) * 10 + ((fileiras / 2 + 1) * assentosPorFileira) * 8;
+			}
+		}
+
+		System.out.println("Total: ");
+		System.out.println("R$" + total);
 
 	}
 
